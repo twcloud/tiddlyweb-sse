4d03042487361fa2f8db34c66cdc60b3c0ce2fce
Arlen22 <arlenbee@gmail.com>
2021-07-14 18:56:58 -0400

    Simplify the build process further and add comments

--- build.js
+++ build.js
@@ -3,14 +3,20 @@ var path = require("path");
 var fs = require("fs");
 
 // Pass the command line arguments to the boot kernel
-$tw.boot.argv = ["++dist","."];
-
-// Boot the TW5 app
-$tw.boot.boot();
-
-var filename = path.resolve("output/plugin.info");
-$tw.utils.createFileDirectories(filename);
-var tiddler = JSON.parse($tw.wiki.getTiddlerAsJson("$:/plugins/twcloud/tiddlyweb-sse"));
-var fields = $tw.utils.extend({}, tiddler, JSON.parse(tiddler.text));
-delete fields.text;
-fs.writeFileSync(filename, JSON.stringify(fields, null, "\t"));
+$tw.boot.argv = ["."];
+// we only need the first phase of startup
+$tw.boot.initStartup({});
+// get the bundle output path
+var output = path.resolve("output/plugin.info");
+// create the directory tree
+if(!fs.existsSync(path.dirname(output))) {
+    fs.mkdirSync(path.dirname(output),{recursive: true});
+}
+// load the plugin from the dist folder
+var plugin = $tw.loadPluginFolder(path.join(__dirname,"dist"));
+// put the plugin tiddlers back into the tiddlers field
+$tw.utils.extend(plugin,JSON.parse(plugin.text));
+// remove the text field
+delete plugin.text;
+// write the bundled plugin to file
+fs.writeFileSync(output,JSON.stringify(plugin,null,"\t"));
