2052a66e3931646d45c1e45e7817575b12284c42
Arlen22 <arlenbee@gmail.com>
2023-06-10 11:10:35 -0400

    fix a few things for release

--- build.js
+++ build.js
@@ -7,16 +7,20 @@ $tw.boot.argv = ["."];
 // we only need the first phase of startup
 $tw.boot.initStartup({});
 // get the bundle output path
-var output = path.resolve("plugins/sse/plugin.info");
-// create the directory tree
-if(!fs.existsSync(path.dirname(output))) {
-    fs.mkdirSync(path.dirname(output),{recursive: true});
-}
+var output = [path.resolve("plugins/sse/plugin.info"), path.resolve("plugins/sse/plugin.json")];
+
 // load the plugin from the dist folder
-var plugin = $tw.loadPluginFolder(path.join(__dirname,"dist"));
+var plugin = $tw.loadPluginFolder(path.join(__dirname, "dist"));
 // put the plugin tiddlers back into the tiddlers field
-$tw.utils.extend(plugin,JSON.parse(plugin.text));
+$tw.utils.extend(plugin, JSON.parse(plugin.text));
 // remove the text field
 delete plugin.text;
 // write the bundled plugin to file
-fs.writeFileSync(output,JSON.stringify(plugin,null,"\t"));
+output.forEach(output => {
+    // create the directory tree
+    if (!fs.existsSync(path.dirname(output))) {
+        fs.mkdirSync(path.dirname(output), { recursive: true });
+    }
+    fs.writeFileSync(output, JSON.stringify(plugin, null, "\t"));
+})
+
